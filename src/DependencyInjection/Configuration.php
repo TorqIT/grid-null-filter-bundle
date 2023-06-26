96a2d743af1c934a99792794608cb5019d22766e
Jeremy Noonan <jeremy.noonan@torqit.ca>
2023-06-26 13:08:50 -0300

    fixed extension

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('grid_null_filter');
+        $treeBuilder = new TreeBuilder('torq_it_grid_null_filter');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- src/DependencyInjection/TorqITGridNullFilterExtension.php
+++ src/DependencyInjection/TorqITGridNullFilterExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class GridNullFilterExtension extends Extension
+class TorqITGridNullFilterExtension extends Extension
 {
     /**
      * {@inheritdoc}
