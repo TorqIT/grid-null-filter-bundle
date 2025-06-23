de46ceafb30cc9f098c901b52e3fb5aad2b12d68
cameronfromtorq <105230931+cameronfromtorq@users.noreply.github.com>
2025-06-23 09:51:09 -0300

    fix bundle to work on pimcore 11

--- src/Resources/public/js/pimcore/startup.js
+++ src/Resources/public/js/pimcore/startup.js
@@ -1,106 +1,99 @@
 pimcore.registerNS("pimcore.plugin.TorqITGridNullFilterBundle");
 
-pimcore.plugin.TorqITGridNullFilterBundle = Class.create(pimcore.plugin.admin, {
-    getClassName: function () {
-        return "pimcore.plugin.TorqITGridNullFilterBundle";
-    },
+pimcore.plugin.TorqITGridNullFilterBundle = Class.create({
+  initialize: function () {
+    Ext.on("added", this.addNullFilterToGrid.bind(this));
+  },
 
-    initialize: function () {
-        pimcore.plugin.broker.registerPlugin(this);
-    },
-
-    pimcoreReady: function (params, broker) {
-        // alert("TorqITGridNullFilterBundle ready!");
+  addNullFilterToGrid: function (e) {
+    if (
+      e.xtype !== "gridpanel" ||
+      e.bodyCls !== "pimcore_editable_grid" ||
+      !e.cls.includes("pimcore_object_grid_panel")
+    ) {
+      return;
     }
+
+    const gridStore = e.getStore();
+    gridStore.on("filterchange", (store, filters) =>
+      this.clearActiveClasses(e, store, filters)
+    );
+
+    const menu = e.headerCt.getMenu();
+
+    menu.on("beforeshow", this.setCheckboxState.bind(this));
+
+    menu.add({
+      xtype: "menucheckitem",
+      name: "isNullOrEmpty",
+      text: "Filter For Null/Empty",
+      handler: function (m) {
+        const column = menu.activeHeader;
+        let dataIndex = column.dataIndex;
+        const type = column?.filter?.type;
+
+        if (type === "quantityValue") {
+          dataIndex = dataIndex + "__value";
+        }
+
+        if (m.checked) {
+          const nullFilter = new Ext.util.Filter({
+            dataIndex: dataIndex,
+            property: dataIndex,
+            // value: 'NULL', Do not pass value to bypass pimcore filter processing, with a value a condition will be added which we need to override
+            operator: "",
+            type: "isNullOrEmpty",
+          });
+
+          gridStore.addFilter([nullFilter]);
+          column.filter.setColumnActive(true);
+        } else {
+          const nullFilter = this.findFilter(
+            gridStore.getFilters(),
+            dataIndex,
+            "isNullOrEmpty"
+          );
+          if (nullFilter) gridStore.removeFilter(nullFilter);
+          column.filter.setColumnActive(false);
+        }
+      }.bind(this),
+    });
+  },
+
+  clearActiveClasses: function (grid, store, filters) {
+    if (filters && filters.length === 0) {
+      const columns = grid.getColumns();
+
+      for (const column of columns) {
+        if (!column.filter) continue;
+        column.filter.setColumnActive(false);
+      }
+    }
+  },
+
+  setCheckboxState: function (e) {
+    const column = e.up();
+    const columnName = column.config.dataIndex;
+    const store = column.up().up().store;
+    const filters = store.getFilters();
+
+    const matchedFilter = this.findFilter(filters, columnName, "isNullOrEmpty");
+    const isChecked = !!matchedFilter;
+
+    const nullCheckMenuITem = e.down("[text=Filter For Null/Empty]");
+    nullCheckMenuITem.setChecked(isChecked);
+  },
+
+  findFilter: function (filters, columnName, type) {
+    for (const item of filters.items) {
+      if (item.type === type && item.getProperty() === columnName) {
+        return item;
+      }
+    }
+
+    return null;
+  },
 });
 
-const TorqITGridNullFilterBundlePlugin = new pimcore.plugin.TorqITGridNullFilterBundle();
-
-(function InitializeGridNullFilter() {
-    const IsNullName = 'isNullOrEmpty';
-
-    const addNullFilterToGrid = function (e) {
-
-        if (e.xtype !== 'gridpanel' || e.bodyCls !== 'pimcore_editable_grid' || !e.cls.includes("pimcore_object_grid_panel") ) {  // e.getStore().getProxy().type != 'ajax') {
-            return;
-        }
-
-        const gridStore = e.getStore();
-        gridStore.on('filterchange', (store, filters) => clearActiveClasses(e, store, filters));
-
-        const menu = e.headerCt.getMenu();
-
-        menu.on('beforeshow', setCheckboxState);
-
-        menu.add({
-            xtype: 'menucheckitem',
-            name: IsNullName,
-            text: 'Filter For Null/Empty',
-            handler: function (m) {
-
-                const column = menu.activeHeader;
-                let dataIndex = column.dataIndex;
-                const type = column?.filter?.type;
-
-                if (type === "quantityValue") {
-                    dataIndex = dataIndex+"__value";
-                }
-
-                if (m.checked) {
-                    const nullFilter = new Ext.util.Filter({
-                        dataIndex: dataIndex,
-                        property: dataIndex,
-                        // value: 'NULL', Do not pass value to bypass pimcore filter processing, with a value a condition will be added which we need to override
-                        operator: '',
-                        type: 'isNullOrEmpty',
-                    });
-
-                    gridStore.addFilter([nullFilter]);
-                    column.filter.setColumnActive(true);
-
-                } else {
-                    const nullFilter = findFilter(gridStore.getFilters(), dataIndex, IsNullName);
-                    if (nullFilter) gridStore.removeFilter(nullFilter);
-                    column.filter.setColumnActive(false);
-                }
-            }
-        });
-    };
-
-    const clearActiveClasses = function (grid, store, filters) {
-            if (filters && filters.length === 0) {
-                const columns = grid.getColumns();
-
-                for (const column of columns) {
-                    if (!column.filter)
-                        continue;
-                    column.filter.setColumnActive(false);
-                }
-        }
-    }
-
-    const setCheckboxState = function (e) {
-        const column = e.up();
-        const columnName = column.config.dataIndex;
-        const store = column.up().up().store;
-        const filters = store.getFilters();
-
-        const matchedFilter = findFilter(filters, columnName, IsNullName)
-        const isChecked = !!matchedFilter;
-
-        const nullCheckMenuITem = e.down("[text=Filter For Null/Empty]");
-        nullCheckMenuITem.setChecked(isChecked);
-    }
-
-    const findFilter = function(filters, columnName, type) {
-        for (const item of filters.items) {
-            if (item.type === type && item.getProperty() === columnName) {
-                return item;
-            }
-        }
-
-        return null;
-    }
-
-    Ext.on('added', addNullFilterToGrid);
-})();
+const TorqITGridNullFilterBundlePlugin =
+  new pimcore.plugin.TorqITGridNullFilterBundle();
--- src/TorqITGridNullFilterBundle.php
+++ src/TorqITGridNullFilterBundle.php
@@ -3,13 +3,29 @@
 namespace TorqIT\GridNullFilterBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
 
-class TorqITGridNullFilterBundle extends AbstractPimcoreBundle
+class TorqITGridNullFilterBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
     public function getJsPaths(): array
     {
         return [
             '/bundles/torqitgridnullfilter/js/pimcore/startup.js'
         ];
+    }
+
+    public function getCssPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeCssPaths(): array
+    {
+        return [];
     }
 }
